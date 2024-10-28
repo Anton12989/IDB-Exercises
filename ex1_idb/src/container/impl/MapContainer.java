793fd08919aa9cb466233b682ec91dab689c6b61
TopTargaryen <42695284+thomasjohanjojo@users.noreply.github.com>
2024-10-28 22:28:15 +0100

    Implemented some functions in part -a of Task 1.2
    Implemented every function (using java.List as the underlying data structure) except the function getMetaData() and the constructor of the class.

--- ex1_idb/src/container/impl/MapContainer.java
+++ ex1_idb/src/container/impl/MapContainer.java
@@ -3,10 +3,21 @@ package container.impl;
 import container.Container;
 import util.MetaData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.NoSuchElementException;
 
 public class MapContainer<Value> implements Container<Long, Value> {
-	
+	private boolean isContainerOpen = false;
+	private enum statusOfKeys  {reserved, used, removed};
+	private List<statusOfKeys> listOfKeys = new ArrayList<>();
+	private List<Value> listOfValues = new ArrayList<>();
+	private int lastUsedKey;
+	private int sizeOfContainer;
+
+
+
 	public MapContainer() {
 		// TODO
 	}
@@ -20,33 +31,84 @@ public class MapContainer<Value> implements Container<Long, Value> {
 	@Override
 	public void open() {
 		// TODO
+		if(isContainerOpen == false)
+		{
+			isContainerOpen = true;
+		}
 	}
 
 	@Override
 	public void close() {
 		// TODO
+		if(isContainerOpen == true)
+		{
+			isContainerOpen = false;
+		}
 	}
 	
 	@Override
 	public Long reserve() throws IllegalStateException {
 		// TODO
-		return null;
+		if(isContainerOpen == false){
+			throw new IllegalStateException();
+		}
+		if((lastUsedKey + 1) == sizeOfContainer){
+			throw new RuntimeException("No more new keys available in list of keys");
+		}
+		Long newKey = (long) (lastUsedKey + 1);
+		lastUsedKey = lastUsedKey + 1;
+		listOfKeys.add(Math.toIntExact(newKey), statusOfKeys.reserved);
+		return newKey;
 	}
 	
 
 	@Override
 	public Value get(Long key) throws NoSuchElementException {
 		// TODO
-		return null;
+		if(isContainerOpen == false){
+			throw new IllegalStateException();
+		}
+		if(listOfKeys.get(Math.toIntExact(key)) == statusOfKeys.used){
+			return listOfValues.get(Math.toIntExact(key));
+		}else{
+			throw new NoSuchElementException();
+		}
 	}
 
 	@Override
 	public void update(Long key, Value value) throws NoSuchElementException {
 		// TODO
+		if(isContainerOpen == false){
+			throw new IllegalStateException();
+		}
+		if(key > lastUsedKey){
+			throw new NoSuchElementException();
+		} else if(listOfKeys.get(Math.toIntExact(key)) == statusOfKeys.reserved){
+			listOfValues.add(Math.toIntExact(key), value);
+		} else if(listOfKeys.get(Math.toIntExact(key)) == statusOfKeys.used){
+			listOfValues.set(Math.toIntExact(key), value);
+		} else if(listOfKeys.get(Math.toIntExact(key)) == statusOfKeys.removed){
+			throw new NoSuchElementException("Key was deleted");
+		}
+
+
 	}
 
 	@Override
 	public void remove(Long key) throws NoSuchElementException {
 		// TODO
+		if(isContainerOpen == false){
+			throw new IllegalStateException();
+		}
+		if(listOfKeys.get(Math.toIntExact(key)) == statusOfKeys.removed){
+			throw new NoSuchElementException("Value was removed already");
+		} else if(listOfKeys.get(Math.toIntExact(key)) == statusOfKeys.reserved){
+			throw new NoSuchElementException();
+		} else if(listOfKeys.get(Math.toIntExact(key)) == statusOfKeys.used)
+		{
+			listOfValues.remove(Math.toIntExact(key));
+			listOfKeys.set(Math.toIntExact(key), statusOfKeys.reserved);
+
+		}
 	}
 }
